ba7a836faf282c6568d5bfa917200ce8dd3b62e2
SDL2 <salvadordeleon2@gmail.com>
2019-07-16 03:59:18 -0600

    toggle amborguesa

--- scrpt/utils.js
+++ scrpt/utils.js
@@ -1,6 +1,14 @@
 
 
-
-
 //alert('mmmmmmm');
+
+//toggle de la burger 
+document.getElementById('nav-toggle').onclick = function(){
+    var $togflex = document.getElementById('ulflex');
+    document.getElementById("nav-content").classList.toggle("hidden");
+    $togflex.classList.toggle('flex');
+}
+
+
+
 
